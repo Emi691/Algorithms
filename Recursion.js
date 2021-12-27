39a89fc6d5eb6c5176e0a9be26d5990b690dca34
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-12-26 16:26:52 -0800

    redefines recursive function to count each recursion

--- Recursion.js
+++ Recursion.js
@@ -1,8 +1,8 @@
-function recursive(i){
-    console.log(`recursive loop #${i}`)
-    if(i <= 0) return
-    recursive(i - 1)
+function recursive(i, j){
+    console.log(`call #${i}`)
+    if(i >= j) return
+    recursive(i+1, j)
 }
 
 
-recursive(5)
+recursive(1, 5)
