d6499edbf4ee8c51d49554ba841ac84b2600aef4
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-12-26 19:47:41 -0800

    refactors quicksort function to be more efficient

--- Quicksort.js
+++ Quicksort.js
@@ -1,10 +1,11 @@
 function quicksort(array) {
-    if(array.length < 2){
-        return array
-    }else{
-        let pivot = array[0]
-        let less = array.filter(n => n < pivot)
-        let more = array.filter(n => n > pivot)
-        return quicksort(less).concat(pivot).concat(quicksort(more))   
-    }
+    if(array.length < 2)return array
+
+    let middle = Math.floor(array.length/2)
+    let pivot = array[middle]
+    let less = array.filter(n => n < pivot)
+    let more = array.filter(n => n > pivot)
+    return quicksort(less).concat(pivot).concat(quicksort(more))   
 }
+
+
