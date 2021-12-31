2311b33fae37813c47cb85b9a23489efd1f47819
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-12-31 11:24:15 -0800

    refactors sum and items functions and removes test code

--- DivideConquer.js
+++ DivideConquer.js
@@ -1,13 +1,11 @@
-let array = [2, 3, 5, 1, 6, 4]
-
 function sum(array){
   if(array.length < 1) return 0
-  return array[0] + sum(array.splice(1))
+  return array[0] + sum(array.slice(1))
 }
 
 function items(array){
   if(array.length < 1) return 0
-  return 1 + items(array.splice(1))
+  return 1 + items(array.slice(1))
 }
 
 function maxValue(array){
@@ -15,5 +13,3 @@ function maxValue(array){
   if(array.length < 1) return 0
   return array[0] > maxValue(array.slice(1)) ? array[0] : maxValue(array.slice(1))
 }
-
-console.log(maxValue(array))
