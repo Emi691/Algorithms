3c6814c74f3ca13cbdd0e6d870afb0afece69a3b
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2022-01-09 20:41:45 -0800

    defines max2 function and renames max function fo max1

--- DivideConquer.js
+++ DivideConquer.js
@@ -8,8 +8,12 @@ function count(array){
   return 1 + items(array.slice(1))
 }
 
-function max(array){
+function max1(array){
   if(array.length < 2) return array[0]
   if(array.length < 1) return 0
   return array[0] > max(array.slice(1)) ? array[0] : max(array.slice(1))
+}
+
+function max2(array){
+  
 }
